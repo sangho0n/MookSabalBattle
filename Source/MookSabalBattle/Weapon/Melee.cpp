fdb83fc2461e4999da499e1424ae59cea2a57593
sanghoon_lab <tkdgns0917@gmail.com>
2023-08-21 15:06:53 +0900

    add Melee class
    한손 무기류

---
+++ Source/MookSabalBattle/Weapon/Melee.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Melee.h"
+
+AMelee::AMelee() : Super()
+{
+
+}
+
+void AMelee::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+}
---
+++ Source/MookSabalBattle/Weapon/Melee.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "../MookSabalBattle.h"
+#include "Weapon.h"
+#include "Melee.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract)
+class MOOKSABALBATTLE_API AMelee : public AWeapon
+{
+	GENERATED_BODY()
+
+public:
+	AMelee();
+
+	virtual void PostInitializeComponents() override;
+	
+};
