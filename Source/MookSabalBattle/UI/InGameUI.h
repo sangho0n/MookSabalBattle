1b8be8591425b01a84d7f06722a78662e18a033a
sanghoon_lab <tkdgns0917@gmail.com>
2023-08-23 10:55:16 +0900

    add InGame UI class and config UMG

---
+++ Source/MookSabalBattle/UI/InGameUI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "../MookSabalBattle.h"
+#include "Blueprint/UserWidget.h"
+#include "InGameUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOOKSABALBATTLE_API UInGameUI : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
