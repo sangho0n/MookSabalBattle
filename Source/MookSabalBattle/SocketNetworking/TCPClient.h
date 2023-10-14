02491f1d12a72ab75ae17613a865fb66ba979457
sanghoon_lab <tkdgns0917@gmail.com>
2023-10-14 15:57:18 +0900

    add tcp client socket class

---
+++ Source/MookSabalBattle/SocketNetworking/TCPClient.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "../MookSabalBattle.h"
+#include "TCPSocketBase.h"
+#include "TCPClient.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOOKSABALBATTLE_API UTCPClient : public UTCPSocketBase
+{
+	GENERATED_BODY()
+
+public:
+	void Join(FString ServerIP);
+};
