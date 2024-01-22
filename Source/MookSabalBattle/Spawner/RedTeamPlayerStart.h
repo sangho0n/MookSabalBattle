3605765915080bf9ac27be4a98c740a3e2927773
sanghoon_lab <tkdgns0917@gmail.com>
2024-01-22 20:22:08 +0900

    add player starts and weapon spawner

---
+++ Source/MookSabalBattle/Spawner/RedTeamPlayerStart.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "../MookSabalBattle.h"
+#include "GameFramework/PlayerStart.h"
+#include "RedTeamPlayerStart.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOOKSABALBATTLE_API ARedTeamPlayerStart : public APlayerStart
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/MookSabalBattle/Spawner/WeaponSpawner.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "../MookSabalBattle.h"
+#include "GameFramework/Actor.h"
+#include "WeaponSpawner.generated.h"
+
+UCLASS()
+class MOOKSABALBATTLE_API AWeaponSpawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AWeaponSpawner();
+
+};
